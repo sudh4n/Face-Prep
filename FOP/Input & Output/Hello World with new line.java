ffb6a2cd6fd00bc79e6ca44464cdffed3d5c005b
Sudhan M <98807138+sudh4n@users.noreply.github.com>
2024-05-20 21:33:57 +0530

    Hello World with new line in java

---
+++ FOP/Input & Output/Hello World with new line.java
@@ -0,0 +1,34 @@
+/*Hello World with a new line
+
+
+
+Write a program to print the given sample output.
+
+
+
+Sample Output:
+
+Hello World
+
+Hello World
+
+
+Case 1
+Input (stdin)
+Hello World
+Hello World
+
+Output (stdout)
+Hello World
+Hello World */
+
+//Solution
+
+class Main
+{
+  public static void main(String args[])
+  {
+    //Try out your code here
+    System.out.println("Hello World\nHello World");
+  }
+}
